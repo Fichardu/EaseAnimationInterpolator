19a58e92be9a6c0e40f6e73d0a1a45f0a543c0e6
Fichardu <xufu90@gmail.com>
2015-01-20 18:52:55 +0800

    add Test Activity

--- app/src/main/java/com/fichardu/ease/MainActivity.java
+++ app/src/main/java/com/fichardu/ease/MainActivity.java
@@ -1,17 +1,42 @@
 package com.fichardu.ease;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList.setAdapter(
+                new ArrayAdapter<String>(this, R.layout.drawer_list_item, mInterpolators));
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectDrawerItem(position);
+            }
+        });
+    }
+
+    private void selectDrawerItem(int position) {
+        Log.d("Test", mInterpolators[position]);
+
     }
 
 
@@ -36,4 +61,34 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private static final String[] mInterpolators = new String[]{
+            "EaseInQuadInterpolator",
+            "EaseOutQuadInterpolator",
+            "EaseInOutQuadInterpolator",
+            "EaseInQuartInterpolator",
+            "EaseOutQuartInterpolator",
+            "EaseInOutQuartInterpolator",
+            "EaseInQuintInterpolator",
+            "EaseOutQuintInterpolator",
+            "EaseInOutQuintInterpolator",
+            "EaseInCircInterpolator",
+            "EaseOutCircInterpolator",
+            "EaseInOutCircInterpolator",
+            "EaseInCubicInterpolator",
+            "EaseOutCubicInterpolator",
+            "EaseInOutCubicInterpolator",
+            "EaseInExpoInterpolator",
+            "EaseOutExpoInterpolator",
+            "EaseInOutExpoInterpolator",
+            "EaseInBackInterpolator",
+            "EaseOutBackInterpolator",
+            "EaseInOutBackInterpolator",
+            "EaseInElasticInterpolator",
+            "EaseOutElasticInterpolator",
+            "EaseInOutElasticInterpolator",
+            "EaseInBounceInterpolator",
+            "EaseOutBounceInterpolator",
+            "EaseInOutBounceInterpolator",
+    };
 }
